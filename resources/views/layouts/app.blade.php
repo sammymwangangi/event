3b52120b92c3309afae4880edb5df2d3e38925e5
Sammy Mwangangi <sammymwinzi2@gmail.com>
2020-10-09 12:00:55 +0300

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,9 +74,8 @@
                                     {{ Auth::user()->name }}
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a href="/admin/users" class="dropdown-item">Account Setting</a>
-                                    <a href="/admin/users" class="dropdown-item">Users</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a href="/bookings" class="dropdown-item text-dark">My Bookings</a>
+                                    <a class="dropdown-item text-dark" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                              document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
